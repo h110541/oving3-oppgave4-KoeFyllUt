f3c23ce87db0d79f9cc1369505c696135d78bab4
h110541 <110541@stud.hvl.no>
2021-02-11 00:16:13 +0100

    Endret KlientKoe
    Endret KlientKoe slik at den nå tar i bruk alle de 3 implementasjonene.

--- src/no/hvl/dat102/klient/KlientKoe.java
+++ src/no/hvl/dat102/klient/KlientKoe.java
@@ -3,6 +3,8 @@ package no.hvl.dat102.klient;
 import no.dat102.tabell.TabellKoe;
 import no.hvl.dat102.adt.KoeADT;
 import no.hvl.dat102.exception.EmptyCollectionException;
+import no.hvl.dat102.kjedet.KjedetKoe;
+import no.hvl.dat102.sirkulaer.SirkulaerKoe;
 
 public class KlientKoe {
 
@@ -10,24 +12,36 @@ public class KlientKoe {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		KoeADT<Character> tegnKoe = new TabellKoe<>();
-		//Bytte ut med KjedetKoe og med SirkulaerKoe
+		KoeADT<Character> tegnKoe;
+
+		tegnKoe = new TabellKoe<>();
+		brukKoe(tegnKoe);
+
+		tegnKoe = new KjedetKoe<>();
+		brukKoe(tegnKoe);
+
+		tegnKoe = new SirkulaerKoe<>();
+		brukKoe(tegnKoe);
+	}
+
+	private static void brukKoe(KoeADT<Character> tegnKoe) {
 		String streng = " Denne koen er en FIFO datastruktur.";
 		int lengde = streng.length();
 		for (int i = 0; i < lengde; i++) {
 			tegnKoe.innKoe(streng.charAt(i));
 		}
-		//System.out.println(tegnKoe); // kall på toString()
+
 		try {
 			while (!tegnKoe.erTom()) {
 				char tegn = tegnKoe.utKoe();
 				System.out.print(tegn);
 			}
+			System.out.println();
 		} catch (EmptyCollectionException e) {
 			System.out.print(e.getMessage());
 
 		}
-		
+
 	}
 
 }

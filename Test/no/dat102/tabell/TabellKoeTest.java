eb0ab2170719e787c6440fa7badc9f53731ec9a1
h110541 <110541@stud.hvl.no>
2021-02-10 23:54:23 +0100

    Opprettet TabellKoeTest og SirkulaerKoeTest

---
+++ Test/no/dat102/tabell/TabellKoeTest.java
@@ -0,0 +1,13 @@
+package no.dat102.tabell;
+
+import no.hvl.dat102.adt.KoeADT;
+import no.hvl.dat102.adt.KoeADTTest;
+
+public class TabellKoeTest extends KoeADTTest {
+
+	@Override
+	protected KoeADT<Integer> reset() {
+		return new TabellKoe<Integer>();
+	}
+
+}

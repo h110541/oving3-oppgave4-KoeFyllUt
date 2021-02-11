d8022a617e71b4b3f26144efb16416dd1f3e2269
h110541 <110541@stud.hvl.no>
2021-02-11 01:10:36 +0100

    Lagt til flere tester

--- Test/no/hvl/dat102/adt/KoeADTTest.java
+++ Test/no/hvl/dat102/adt/KoeADTTest.java
@@ -47,4 +47,103 @@ public abstract class KoeADTTest {
 		assertTrue(koe.erTom());
 	}
 
+	@Test
+	public void testInnKoeOgUtKoe() {
+		koe.innKoe(e0);
+		koe.innKoe(e1);
+		koe.innKoe(e2);
+		koe.innKoe(e3);
+		koe.innKoe(e4);
+
+		assertEquals(e0, koe.utKoe());
+		assertEquals(e1, koe.utKoe());
+		assertEquals(e2, koe.utKoe());
+		assertEquals(e3, koe.utKoe());
+		assertEquals(e4, koe.utKoe());
+	}
+
+	@Test
+	public void testInnKoeOgUtKoe2() {
+		koe.innKoe(e1);
+		koe.innKoe(e4);
+		koe.innKoe(e0);
+		koe.innKoe(e3);
+		koe.innKoe(e2);
+
+		assertEquals(e1, koe.utKoe());
+		assertEquals(e4, koe.utKoe());
+		assertEquals(e0, koe.utKoe());
+		assertEquals(e3, koe.utKoe());
+		assertEquals(e2, koe.utKoe());
+	}
+
+	@Test
+	public void testInnKoeOgUtKoe3() {
+		koe.innKoe(e1);
+		koe.innKoe(e1);
+		koe.innKoe(e1);
+
+		assertEquals(e1, koe.utKoe());
+		assertEquals(e1, koe.utKoe());
+		assertEquals(e1, koe.utKoe());
+	}
+
+	@Test
+	public void testInnKoeOgUtKoe4() {
+		koe.innKoe(e4);
+		koe.innKoe(e1);
+		koe.innKoe(e2);
+
+		assertEquals(e4, koe.utKoe());
+		assertEquals(e1, koe.utKoe());
+
+		koe.innKoe(e3);
+		koe.innKoe(e0);
+
+		assertEquals(e2, koe.utKoe());
+		assertEquals(e3, koe.utKoe());
+		assertEquals(e0, koe.utKoe());
+	}
+
+	@Test
+	public void testFoerste() {
+		koe.innKoe(e2);
+		assertEquals(e2, koe.foerste());
+
+		koe.innKoe(e1);
+		koe.innKoe(e1);
+		koe.innKoe(e0);
+
+		assertEquals(e2, koe.foerste());
+
+		koe.utKoe();
+		assertEquals(e1, koe.foerste());
+
+		koe.utKoe();
+		assertEquals(e1, koe.foerste());
+
+		koe.utKoe();
+		assertEquals(e0, koe.foerste());
+	}
+
+	@Test
+	public void testAntall() {
+		assertEquals(0, koe.antall());
+
+		koe.innKoe(e0);
+		assertEquals(1, koe.antall());
+
+		for(int i = 1; i <= 20; i++) {
+			koe.innKoe(e3);
+		}
+		assertEquals(21, koe.antall());
+
+		koe.utKoe();
+		koe.utKoe();
+		assertEquals(19, koe.antall());
+
+		koe.innKoe(e4);
+		assertEquals(20, koe.antall());
+	}
+
 }
